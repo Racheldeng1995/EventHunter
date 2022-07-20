848f584b6cfb3466cb7ebdeae32857ef1b48f7f1
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-20 12:48:00 -0700

    add function on back button

--- assets/js/eventdesc.js
+++ assets/js/eventdesc.js
@@ -1,11 +1,15 @@
 var likeBtn = document.getElementById("like-btn")
+var backBtn = document.getElementById("back-btn")
+var backBtnText = document.getElementById("back-btn-text")
+
+// Add event listener on Like button
+// If click "Like", heart icon turns into solid color, and button text turns into "Liked"
+// If click again, heart icon turns back to default color(transparent with border), and button text turns back to default text("Like")
 likeBtn.addEventListener("click", function(event){
 
     event.preventDefault();
     var likeIcon = document.getElementById("like-icon")
     var likeValue = likeBtn.getAttribute("like-value")
-    console.log(likeValue)
-    
 
     if (likeValue == "like") {
         likeIcon.className = "fa fa-heart"
@@ -20,4 +24,17 @@ likeBtn.addEventListener("click", function(event){
         }
     }
 )
+
+// Add event listener on Back Button
+// !!! Dependency on local storage of Event Main Page
+// !!! Pay attention that not directly link back to Event Main Page, need to link back to "selected"/"already loaded" Event Main Page
+backBtn.addEventListener("click", function(event) {
+
+    event.preventDefault();
+    backBtnText.setAttribute("href", "")
+
+}
+
+)
+
 
