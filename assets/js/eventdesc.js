aff3c70241ffff7c468aaaa4de60920d5233beef
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-21 10:21:56 -0700

    display API data without layout and styling yet

--- assets/js/eventdesc.js
+++ assets/js/eventdesc.js
@@ -83,7 +83,8 @@ var getLocationGeo = function() {
 
     return getEventDetails()
     .then(function(eventDetails) {
-        var eventVenueUrl =  "https://www.eventbriteapi.com/v3/events/" + "338509329517" + "venues"  + "/?token=" + APIToken
+
+        var eventVenueUrl =  "https://www.eventbriteapi.com/v3/" + "venues/"  + eventDetails.location_id + "/?token=" + APIToken
         return fetch(eventVenueUrl)
         .then (function (response) {
             if (response.ok) {              
@@ -93,8 +94,201 @@ var getLocationGeo = function() {
                 alert('Error: Event Not Found');
               }
         })
+        .then(function(data) {
+            var eventLocation = {
+                "address": data.address.localized_address_display,
+                "location_name": data.name,
+                "lat": data.latitude,
+                "lon": data.longitude
+            }
+            console.log(eventLocation)
+
+            return eventLocation
+        })
+
+    .catch(function(error) {
+        alert("Unable to connect to API");
+    });
     }
     )
 }
-getEventDetails()
+
+var getTicketPrice = function() {
+    
+    //var event = getEventDetails()
+    var priceApiUrl = "https://www.eventbriteapi.com/v3/events/" + "345788401417/" + "ticket_classes"   + "/?token=" + APIToken
+
+    return fetch (priceApiUrl)
+    .then (function (response) {
+        if (response.ok) {              
+            return response.json()
+        }
+        else {
+            alert('Error: Event Not Found');
+          }
+    })
+    .then(function(data) {
+        console.log(data)
+
+        var ticketArray = data.ticket_classes
+
+        var keysToKeep = ["cost", "category", "display_name", "on_sale_status"]
+        var redux = array => array.map(o => keysToKeep.reduce((acc, curr) => {
+            acc[curr] = o[curr];
+            return acc;
+        }, {}));
+
+        var ticketDetailsArray = redux(ticketArray) 
+
+        for (i = 0; i < ticketDetailsArray.length; i++) {
+            (function(i) {
+                if(ticketDetailsArray[i].cost == null) {
+                    ticketDetailsArray[i].cost = "$0"
+                }
+                else {
+                    var extrCost = ticketDetailsArray[i].cost.display
+                    ticketDetailsArray[i].cost = extrCost
+                }
+            } (i))
+        }
+
+       var priceDetails = {
+        "price" : ticketDetailsArray
+       }
+
+       console.log(priceDetails)
+       return priceDetails
+    })
+    .catch(function(error) {
+        alert("Unable to connect to API");
+      });
+}
+
+
+var getFullDesc = function() {
+    
+    //var event = getEventDetails()
+    var descApiUrl = "https://www.eventbriteapi.com/v3/events/" + "345788401417/" + "structured_content"   + "/?token=" + APIToken
+
+    return fetch (descApiUrl)
+    .then (function (response) {
+        if (response.ok) {              
+            return response.json()
+        }
+        else {
+            alert('Error: Event Not Found');
+          }
+    })
+    .then(function(data) {
+        console.log(data)
+        var eventFullDesc = {
+            "fulldesc": data.modules[0].data.body.text.replace("<p>", "").replace("</p>", "")
+        }
+
+        return eventFullDesc
+    })
+    .catch(function(error) {
+        alert("Unable to connect to API");
+      });
+}
+
+//getEventDetails();
+//getLocationGeo();
+//getTicketPrice()
+
+var comDetails = function () {
+
+    return getEventDetails()
+    .then(function(eventDetails) {
+        return getLocationGeo()
+        .then(function(eventLocation) {
+            return getFullDesc()
+            .then(function(eventFullDesc) {
+                return getTicketPrice()
+                .then(function(priceDetails) {
+
+                var eventDetailsArray = Object.assign(eventDetails,eventLocation,eventFullDesc,priceDetails)
+                console.log(eventDetailsArray)
+                return eventDetailsArray
+            })
+            }
+        )
+    })
+})
+}
+
+//comDetails();
+var eventMainEl = document.getElementById("event-main")
+genPage = function () {
+
+    return comDetails()
+    .then(function(eventDetailsArray) {
+        console.log(eventDetailsArray)
+        var imgSec = document.createElement("img")
+        imgSec.className = "event-img"
+        imgSec.src = eventDetailsArray.logo_img
+        var eventDetailsMain = document.createElement("section")
+        eventDetailsMain.className = "event-details-main"
+        var eventName = document.createElement("p")
+        eventName.className = "event-name"
+        eventName.innerHTML = eventDetailsArray.title
+        var eventDate = document.createElement("p")
+        eventDate.className = "event-date"
+        eventDate.innerHTML = eventDetailsArray.time
+        var eventLocation = document.createElement("p")
+        eventLocation.className = "event-location"
+        eventLocation.innerHTML = eventDetailsArray.location_name
+        var eventAddress = document.createElement("p")
+        eventAddress.className = "event-address"
+        eventAddress.innerHTML = eventDetailsArray.address
+
+        eventDetailsMain.appendChild(eventName)
+        eventDetailsMain.appendChild(eventDate)
+        eventDetailsMain.appendChild(eventLocation)
+        eventDetailsMain.appendChild(eventAddress)
+
+        var eventPriceLi = document.createElement("ul")
+        eventPriceLi.className = "event-price-main"
+        for (j=0; j <eventDetailsArray.price.length; j++) {
+            (function(j) {
+                var eventPriceEl = document.createElement("li")
+                eventPriceEl.className = "price-detail"
+                var eventPriceCost = document.createElement("p")
+                eventPriceCost.innerHTML = "Price: " + eventDetailsArray.price[j].cost
+                var eventPriceName = document.createElement("p")
+                eventPriceName.innerHTML = "Category: " + eventDetailsArray.price[j].display_name
+                var eventPriceStatus = document.createElement("p")
+                eventPriceStatus.innerHTML = "Status: " + eventDetailsArray.price[j].on_sale_status
+
+                eventPriceEl.appendChild(eventPriceCost)
+                eventPriceEl.appendChild(eventPriceName)
+                eventPriceEl.appendChild(eventPriceStatus)
+
+                eventPriceLi.appendChild(eventPriceEl)
+            }(j))
+        }
+
+        var eventDescription = document.createElement("section")
+        eventDescription.className = "event-desc-main"
+        var eventDescHeader = document.createElement("h1")
+        eventDescHeader.innerHTML = "About this event"
+        var eventDescDetails = document.createElement("p")
+        eventDescDetails.innerHTML = eventDetailsArray.summary
+        var eventFullDescs = document.createElement("P")
+        eventFullDescs.innerHTML = eventDetailsArray.fulldesc
+
+        eventDescription.appendChild(eventDescHeader)
+        eventDescription.appendChild(eventDescDetails)
+        eventDescription.appendChild(eventFullDescs)
+
+        eventMainEl.appendChild(imgSec)
+        eventMainEl.appendChild(eventDetailsMain)
+        eventMainEl.appendChild(eventPriceLi)
+        eventMainEl.appendChild(eventDescription)
+    }
+
+    )
+}
+
+genPage()
 
