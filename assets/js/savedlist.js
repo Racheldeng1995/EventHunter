b48ac0e38c3355f0e04f6fac4a0ba6afb187f865
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-21 15:26:29 -0700

    add function that firect from savelist page to eventdesc page

--- assets/js/savedlist.js
+++ assets/js/savedlist.js
@@ -16,8 +16,12 @@ var genEventList = function(eventlist) {
     var eventTitle = document.createElement("h1")
     eventTitle.innerHTML = eventlist.title
 
-    var eventImg = document.createElement("img")
-    eventImg.src = eventlist.logo_img
+    var eventImg = document.createElement("a")
+    eventImg.href = "./index.html?eventid=" + eventlist.event_id
+    var eventImgEl = document.createElement("img")
+    eventImgEl.src = eventlist.logo_img
+    eventImgEl.className = "event-block-img"
+    eventImg.appendChild(eventImgEl)
     var eventTime = document.createElement("p")
     eventTime.innerHTML = eventlist.time
     var eventLocation = document.createElement("p")
@@ -41,3 +45,4 @@ for (i=0; i < savedItems.length; i++) {
     }(i)
     )
 }
+
