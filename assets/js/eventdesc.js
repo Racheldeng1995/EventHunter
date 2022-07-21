7df971d85f4eea28c4b7ba53312d72ffa562c4c3
Racheldeng1995 <racheldeng1995@gmail.com>
2022-07-20 18:37:11 -0700

    add API data

--- assets/js/eventdesc.js
+++ assets/js/eventdesc.js
@@ -1,3 +1,4 @@
+var APIToken = "ZK55TMREDURZKUJDZZ64"
 var likeBtn = document.getElementById("like-btn")
 var backBtn = document.getElementById("back-btn")
 var backBtnText = document.getElementById("back-btn-text")
@@ -37,4 +38,63 @@ backBtn.addEventListener("click", function(event) {
 
 )
 
+// Grab evrnt id from url query string
+// !!! Dependency on href link from Event Main Page. href should be ./index.html?event=" + eventid
+
+// var getEventId = function() {
+
+//     var queryString = document.location.search;
+//     var eventId = queryString.split("=")[1];
+  
+//     return eventId
+//   };
+ 
+var getEventDetails = function() {
+    
+    //var event = getEventDetails()
+    var eventApiUrl = "https://www.eventbriteapi.com/v3/events/" + "338509329517"   + "/?token=" + APIToken
+
+    return fetch (eventApiUrl)
+    .then (function (response) {
+        if (response.ok) {              
+            return response.json()
+        }
+        else {
+            alert('Error: Event Not Found');
+          }
+    })
+    .then(function(data) {
+        var eventDetails={
+            "summary": data.summary,
+            "logo_img": data.logo.url,
+            "title": data.name.text,
+            "time": moment(data.start.local).format('MMMM Do YYYY, h:mm:ss a'),
+            "location_id": data.venue_id
+        }
+        console.log(eventDetails)
+        return eventDetails
+    })
+    .catch(function(error) {
+        alert("Unable to connect to API");
+      });
+}
+
+var getLocationGeo = function() {
+
+    return getEventDetails()
+    .then(function(eventDetails) {
+        var eventVenueUrl =  "https://www.eventbriteapi.com/v3/events/" + "338509329517" + "venues"  + "/?token=" + APIToken
+        return fetch(eventVenueUrl)
+        .then (function (response) {
+            if (response.ok) {              
+                return response.json()
+            }
+            else {
+                alert('Error: Event Not Found');
+              }
+        })
+    }
+    )
+}
+getEventDetails()
 
