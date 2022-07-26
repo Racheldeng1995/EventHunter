192a90971ffb47668f7ebf78d9c34b39899faf00
Jingsi <gongjingsi@hotmail.com>
2022-07-26 01:34:55 -0400

    update form action

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
+formEl = document.querySelector("#search-form");
 
+var taskFormHandler = function () {
+    event.preventDefault();
+    var timeRangeInput = document.querySelector("select[name='timeRange']").value;
+    var cityInput = document.querySelector("input[name='city']").value;
+
+    console.log(timeRangeInput);
+    console.log(cityInput);
+    // check if input values are empty strings
+    if (!timeRangeInput || !cityInput) {
+        alert("You need to fill out the search form!");
+        return false;
+    }
+    
+    // Redirect to event page
+    window.location.href = 'team.html';
+};
+
+formEl.addEventListener("submit", taskFormHandler);
